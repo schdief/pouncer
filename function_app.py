21cffac1fdb72dfe9584f4ea79e8628d82b953fc
Steve Lohr <schdief.law@gmail.com>
2024-02-20 19:31:46 +0000

    fix payload structure and use auto shutoff of switches

--- function_app.py
+++ function_app.py
@@ -6,6 +6,7 @@ import os
 import datetime
 import pytz
 import time
+import json
 
 #TODO: add schedule to run this every hour between 6 and 20 between may and august
 
@@ -15,7 +16,7 @@ all_device_ids = [
     ['fcb467a5c794', 'East', 'Klavier'],
     ['fcb467a59c88', 'South', 'Essen'],
     ['fcb467a5d358', 'South', 'Wohnen'],
-    ['fcb467a5d358', 'West', 'Terrasse'],
+    ['fcb467a5b4f0', 'West', 'Terrasse'],
     ['fcb467a683c8', 'West', 'Gästebad']
 ]
 
@@ -54,43 +55,30 @@ if 'weather' in weather_data:
     current_hour = current_time.hour
 
     bulk_control_data = []
-    #iterate over the devices and check whether the sun will shine at the current hour and the window is impacted, if so, tell it to close, if not, tell it to open
+    #iterate over the devices and check whether the sun will shine at the current hour and the window is impacted
+    #if so, tell it to close, if not, tell it to open
     for device_id, device_direction, device_name in all_device_ids:
         if device_direction == sun_direction[current_hour] and sunrise_time.hour <= current_hour <= sunset_time.hour:
             logging.info(f'The sun will shine at {current_hour}:00 for {device_name}, closing the blind.')
-            channel = '1'
+            channel = 1
         else:
             logging.info(f'The sun will not shine at {current_hour}:00 for {device_name}, opening the blind.')
-            channel = '0'
+            channel = 0
 
-            payload = {
-                'id': device_id,
-                'channel': channel,
-                'turn': 'on'
-            }
-            #prepare the bulk control request by adding the payload to the list for this device
-            bulk_control_data.append(payload)
+        device_data = {
+            'id': device_id,
+            'channel': channel,
+            'turn': 'on'
+        }
+        #prepare the bulk control request by adding the payload to the list for this device
+        bulk_control_data.append(device_data)
 
-    payload = {
-        'devices': bulk_control_data,
-        'auth_key': shelly_auth
-    }
-    bulk_control_data.append(payload)
+    # Convert the payload to the desired format
+    payload_str = f"devices={json.dumps(bulk_control_data)}&auth_key={shelly_auth}"
+    logging.info(f'Payload:\n{payload_str}')
 
-    response = requests.post(f'https://shelly-27-eu.shelly.cloud/device/relay/bulk_control', json=bulk_control_data)
-
-    if response.status_code == 200:
-        logging.info('Bulk control request sent successfully.')
-    else:
-        logging.error(f'Failed to send bulk control request. Error: {response.text}')
-
-    #wait for 60 seconds to let the blinds close
-    time.sleep(60)
-    #turn off all devices so they can be controlled again
-    for data in bulk_control_data:
-        data['turn'] = 'off'
-
-    response = requests.post(f'https://shelly-27-eu.shelly.cloud/device/relay/bulk_control', json=bulk_control_data)
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    response = requests.post(f'https://shelly-27-eu.shelly.cloud/device/relay/bulk_control', data=payload_str, headers=headers)
 
     if response.status_code == 200:
         logging.info('Bulk control request sent successfully.')
