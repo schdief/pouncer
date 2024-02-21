f8124871d3acd0583e00810b89e5f09c1b644cbc
Steve Lohr <schdief.law@gmail.com>
2024-02-21 19:04:18 +0000

    use single calls instead of bulk

--- function_app.py
+++ function_app.py
@@ -6,7 +6,6 @@ import os
 import datetime
 import pytz
 import time
-import json
 
 #TODO: add schedule to run this every hour between 6 and 20 between may and august
 
@@ -39,53 +38,49 @@ sun_direction = {
     20: 'West'
 }
 
-shelly_auth = os.getenv('SHELLY_AUTH_TOKEN')
+# prepare weather forecast request
+berlin_tz = pytz.timezone('Europe/Berlin')
+current_time = datetime.datetime.now(tz=berlin_tz)
+current_hour = current_time.hour
+city = 'Schmölln-Putzkau, DE'
+logging.info(f'Checking sunshine forecat for {city} at {current_hour}.')
 
 # get the weather forecast for the current hour, to see whether the sun will shine
 api_key = os.getenv('OPENWEATHERMAP_API_KEY')
-city = 'Schmölln-Putzkau, DE'
 response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}')
 weather_data = response.json()
 
 if 'weather' in weather_data:
-    berlin_tz = pytz.timezone('Europe/Berlin')
     sunrise_time = datetime.datetime.fromtimestamp(weather_data['sys']['sunrise'], tz=berlin_tz)
     sunset_time = datetime.datetime.fromtimestamp(weather_data['sys']['sunset'], tz=berlin_tz)
-    current_time = datetime.datetime.now(tz=berlin_tz)
-    current_hour = current_time.hour
-
-    bulk_control_data = []
+    shelly_auth = os.getenv('SHELLY_AUTH_TOKEN')
     #iterate over the devices and check whether the sun will shine at the current hour and the window is impacted
     #if so, tell it to close, if not, tell it to open
     for device_id, device_direction, device_name in all_device_ids:
         if device_direction == sun_direction[current_hour] and sunrise_time.hour <= current_hour <= sunset_time.hour:
             logging.info(f'The sun will shine at {current_hour}:00 for {device_name}, closing the blind.')
-            channel = 1
+            channel = "1"
         else:
             logging.info(f'The sun will not shine at {current_hour}:00 for {device_name}, opening the blind.')
-            channel = 0
+            channel = "0"
 
-        device_data = {
-            'id': device_id,
-            'channel': channel,
-            'turn': 'on'
-        }
-        #prepare the bulk control request by adding the payload to the list for this device
-        bulk_control_data.append(device_data)
+        # prepare device payload
+        payload_str = f"channel={channel}&turn=on&id={device_id}&auth_key={shelly_auth}"
+        logging.info(f'Payload: {payload_str}')
 
-    # Convert the payload to the desired format
-    payload_str = f"devices={json.dumps(bulk_control_data)}&auth_key={shelly_auth}"
-    logging.info(f'Payload:\n{payload_str}')
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        response = requests.post(f'https://shelly-27-eu.shelly.cloud/device/relay/control', data=payload_str, headers=headers)
 
-    headers = {'content-type': 'application/x-www-form-urlencoded'}
-    response = requests.post(f'https://shelly-27-eu.shelly.cloud/device/relay/bulk_control', data=payload_str, headers=headers)
-
-    if response.status_code == 200:
-        logging.info('Bulk control request sent successfully.')
-    else:
-        logging.error(f'Failed to send bulk control request. Error: {response.text}')
+        if response.status_code == 200:
+            logging.info(f'Request sent successfully for {device_name}.')
+        else:
+            logging.error(f'Failed to control {device_name}. Error: {response.text}')
+        
+        time.sleep(10) #wait for 10 second before sending the next request to avoid rate limit
     
+    logging.info('All blinds set according to forecast, see you next hour.')
+
 else:
-    logging.error('Failed to fetch or read weather data.')    
-    
+    logging.error('Failed to fetch or read weather data - mission abort.')    
+
 #TODO: send a mail or telegram on error
